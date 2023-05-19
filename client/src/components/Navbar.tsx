f5bfea662e46f4c1f984eb638f283913795cca9e
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-19 20:16:13 +0200

    Create navbar component

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { IoMdAdd, IoMdSearch } from 'react-icons/io'
+import { User } from '../interfaces'
+
+interface NavbarProps {
+	searchTerm: string
+	setSearchTerm: React.Dispatch<React.SetStateAction<string>>
+	user: User
+}
+
+const Navbar = ({ searchTerm, setSearchTerm, user }: NavbarProps) => {
+	const navigate = useNavigate()
+
+	if (!user) return null
+
+	return (
+		<div className='mt-5 flex w-full gap-2 pb-7 md:gap-5'>
+			<div className='flex w-full items-center justify-start rounded-md border-none bg-white px-2 outline-none focus-within:shadow-sm'>
+				<IoMdSearch fontSize={21} className='ml-1' />
+				<input
+					type='text'
+					onChange={(e) => setSearchTerm(e.target.value)}
+					placeholder='Search'
+					value={searchTerm}
+					onFocus={() => navigate('/search')}
+					className='w-full bg-white p-2 outline-none'
+				/>
+			</div>
+			<div className='flex gap-3'>
+				<Link to={`/user-profile/${user?.id}`} className='hidden md:block'>
+					<img
+						src={user?.imagePath}
+						alt='user'
+						className='w-14 h-12 rounded-lg'
+					/>
+				</Link>
+				<Link
+					to='/create-pin'
+					className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
+				>
+					<IoMdAdd />
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
